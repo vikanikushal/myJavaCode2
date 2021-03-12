85c37fd93d2857912975208af735545ad5444dfb
vikanikushal <vikanikushal123@gmail.com>
2021-03-12 18:49:01 +0530

    commit 2

--- src/problem2.java
+++ src/problem2.java
@@ -16,11 +16,10 @@ import java.util.Arrays;
 public class problem2 {
 
     public static void main(String... args) {
-        System.out.println(Arrays.toString(product(new int[] { 1, 2, 3, 4, 5 })));
-        System.out.println(Arrays.toString(product(new int[] { 3, 2, 1 })));
+        System.out.println(Arrays.toString(solution(new int[] { 1, 2, 3, 4, 5 })));
     }
 
-    public static int[] product(int[] arr) {
+    public static int[] solution(int[] arr) {
         if (arr == null || arr.length < 2)
             return null;
 
